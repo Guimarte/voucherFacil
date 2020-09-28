9095789d6b38143d5c4e4a20fe947fbbfcce3490
Guilherme Martins <guimartinsstyle@gmail.com>
2020-09-28 00:11:10 -0300

    Projeto Voucher Facil 1.0

---
+++ codigo.js
@@ -0,0 +1,33 @@
+function array(a,b){
+    a = document.getElementById('campoN1').value
+    b = document.getElementById('campoN2').value
+    n1 = Number.parseInt(a)
+    n2 = Number.parseInt(b)
+
+    console.log(n1,n2)
+    let vouchers = []
+    for(inicio = n1; inicio<=n2; inicio++){
+        console.log(inicio)
+    
+    vouchers.push(inicio)
+    
+    }
+    console.log(vouchers)
+}
+
+let botaoVoucher = document.getElementById('botaoAddVouchers').addEventListener('click',array)
+
+function voucherID(){
+    let cadastroClienteVoucher = [{}]
+    let nomedoCliente = document.getElementById('nomeCliente').value
+    let NumerodoVoucher = document.getElementById('voucherCliente').value
+    let voucherConvertido = Number.parseInt(NumerodoVoucher)
+    
+    cadastroClienteVoucher.nome = nomedoCliente
+    cadastroClienteVoucher.voucher = voucherConvertido
+    console.log(cadastroClienteVoucher)
+    
+    
+}
+
+let botaoVoucherID = document.getElementById('botaoVouchersId').addEventListener('click',voucherID)
